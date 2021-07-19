71ad3961da6901c2fb718f47a1183dc40dd16e0f
Yakuza <86195673+YAKUZAG@users.noreply.github.com>
2021-07-19 17:26:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+mongoose
+    .connect(
+        'mongodb+srv://dula:12345@cluster0.zpphj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(() => {
+        console.log("MongoDB Connected");
+    });
+
+    app.get('/',(req,res)=>{
+        res.send('<b>ITP Backend API</b>')
+    });
+
+//http://localhost:5000/api/student
+app.use("/api/student", require("./routes/Student.route"))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server is Running...");
+});
